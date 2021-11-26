9295ab9231168730cbd4c112cc06662928098e23
DasRosa <luhuyen108@gmail.com>
2021-11-26 14:14:21 +0700

    Update UI for Contact

--- client/src/components/body/Contact/Contact.js
+++ client/src/components/body/Contact/Contact.js
@@ -1,41 +1,52 @@
 import React from 'react'
 import './Contact.css'
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-import phone from "../../../Image/phone.png";
-import mail from "../../../Image/mail.png";
-import location from "../../../Image/location.png";
-import facebook from "../../../Image/facebook.png";
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailIcon from '@mui/icons-material/Mail';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import { IconButton } from '@mui/material';
 
 
 function contact() {
     return (
-        <><div className="contactus-container">
-            <div className="contact-contaier">
-                <h3 name="contactus"><span>Contact Us</span></h3>
-                <h2 name="bookstore"><span>BookStore</span></h2>
-            </div>
-            <div className="contactlist-links">
+        <div className="contactus-container">
+            <h3 className="contactus">Contact Us</h3>
+            <div className="contact-container">
                 <div className="contactlist-wrapper">
                     <div className="contactlist-items">
-                        <h3>Address</h3>
-                        <span>18 Hoang Quoc Viet, Cau Giay District, Hanoi</span>
+                        <h4><IconButton color="primary" href="https://www.google.com/maps/place/18B+Ho%C3%A0ng+Qu%E1%BB%91c+Vi%E1%BB%87t,+Ngh%C4%A9a+%C4%90%C3%B4,+C%E1%BA%A7u+Gi%E1%BA%A5y,+H%C3%A0+N%E1%BB%99i,+Vietnam/@21.0464781,105.8000449,17z/data=!3m1!4b1!4m5!3m4!1s0x3135ab31c7ad04ff:0x8f85621b80682d05!8m2!3d21.0464731!4d105.8022389"><LocationOnIcon/></IconButton>Address</h4>
+                        <p>18 Hoang Quoc Viet, Cau Giay District, Hanoi</p>
                     </div>
                     <div className="contactlist-items">
-                        <h3>Email</h3>
-                        <span>officeusth@usth.edu.vn</span>
+                        <h4><IconButton color="primary"><MailIcon /></IconButton>Email</h4>
+                        <p>officeusth@usth.edu.vn</p>
                     </div>
                     <div className="contactlist-items">
-                        <h3>Phone Number</h3>
-                        <span>+84-24 37 91 69 60</span>
+                        <h4><IconButton color="primary"><PhoneIcon /></IconButton>Phone Number</h4>
+                        <p>+84-24 37 91 69 60</p>
                     </div>
+                    <div className="contactlist-items">
+                        <h4><IconButton color="primary" href="https://www.facebook.com/usth.edu.vn"><FacebookRoundedIcon/></IconButton>Facebook</h4>
+                        <p>https://www.facebook.com/usth.edu.vn</p>
+                    </div>
+                    <div className="contactlist-items">
+                        <h4><IconButton color="primary" href="https://www.instagram.com/usth_official"><InstagramIcon/></IconButton>Instagram</h4>
+                        <p>https://www.instagram.com/usth_official</p>
+                    </div>
+                    <div className="contactlist-items">
+                        <h4><IconButton color="primary" href="https://www.youtube.com/channel/UCqc5GUt4MW5ScbV7XqUWhiQ"><YouTubeIcon/></IconButton>YouTube</h4>
+                        <p>ĐH Khoa học và Công nghệ Hà Nội -USTH</p>
+                    </div>
+                </div>
+                <div>
+                    <iframe className="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.645246689171!2d105.79815151533232!3d21.04687599253463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3b5f358169%3A0x90bc69ef0ad4355d!2zMTggSG_DoG5nIFF14buRYyBWaeG7h3QsIE5naMSpYSDEkMO0LCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1637502459268!5m2!1svi!2s" width="600" height="450" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 </div>
             </div>
         </div>
-            <div>
-                <iframe className="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.645246689171!2d105.79815151533232!3d21.04687599253463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3b5f358169%3A0x90bc69ef0ad4355d!2zMTggSG_DoG5nIFF14buRYyBWaeG7h3QsIE5naMSpYSDEkMO0LCBD4bqndSBHaeG6pXksIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1637502459268!5m2!1svi!2s" width="600" height="450" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div></>
-
     )
 }
 
--- client/src/components/body/Profile/Profile.js
+++ client/src/components/body/Profile/Profile.js
@@ -18,38 +18,6 @@ const Profile = () => {
                     <div>
                         <span className="info-head">Name: </span>{currentUser.name}
                     </div>
-<<<<<<< HEAD
-                    <div className="customeredit-info">
-                        <div className="name-container">
-                            <span >Name: </span>
-                            <input className='formedit-name' name='name' type='name' placeholder='Lý Anh Kiệt' />
-                        </div>
-                        <div className="phonenumber-container">
-                            <span>Phone Number: </span>
-                            <input className='formedit-phonenumber' name='phonenumber' type='phonenumber' placeholder='12345678' />
-                        </div>
-                        <div>
-                            <span className="email-container">Email: </span>
-                            <input className='formedit-email' name='email' type='email' placeholder='@gmail.com' />
-                        </div>
-                        <div>
-                            <span className="address-container">Address: </span>
-                            <input className='formedit-address' name='address' type='address' placeholder='Ha Noi' />
-                        </div>
-                        <span className="gender-container">Gender: </span>
-                        <label className="radio-button">Male
-                            <input type="radio" checked="checked" name="radio" />
-                            <span class="checkmark"></span>
-                        </label>
-                        <label className="radio-button">
-                            <input type="radio" name="radio" />Female
-                            <span class="checkmark"></span>
-                        </label>
-                        <label className="radio-button">Other
-                            <input type="radio" name="radio" />
-                            <span class="checkmark"></span>
-                        </label>
-=======
                     <div><span className="info-head">Phone Number: </span>{currentUser.phoneNumber}
                     </div>
                     <div>
@@ -96,7 +64,6 @@ const Profile = () => {
                     </div>
                     <div className="edit-container">
                         <div className="edit-head">Date Of Birth: </div>
->>>>>>> 3b73eb7132ede99c5fc20cf2dd2981a30683d761
                         <div>
                         <select className="dateofbirth" name="day"><option value="0">Day</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
                         <select className="dateofbirth" name="month"><option value="0">Month</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select>
