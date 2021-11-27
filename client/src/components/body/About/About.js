015f7a009e8956200ccb6df8c635a575df080507
KietLyanh <lyanhkiet2111@gmail.com>
2021-11-27 15:50:52 +0700

    Update About

--- client/src/components/body/About/About.js
+++ client/src/components/body/About/About.js
@@ -1,67 +1,42 @@
 import React from "react";
 import './About.css'
-import avatar from "../../../Image/avatar.jpg";
 
 function About() {
     return (
-        <div className="about-links">
-            <div className="aboutinfo-wrapper">
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
-                <div className="about-container">
-                    <img className="about-image" src={avatar} alt="Flowers in Chania" />
-                    <div className="about-items">
-                        <span>Name: </span>
-                        <span>Date Of Birth: </span>
-                        <span>Major: </span>
-                    </div>
-                </div>
+        <div className="about-container">
+            <h1>About Us</h1>
+            <div className="about-header">
+                <strong>Nguồn nhân lực</strong>
+            </div>
+            <div className="about-content">
+                <p>Để xây dựng Thương hiệu mạnh, một trong những định hướng quan trọng hàng đầu của FAHASA là chiến lược phát triển nguồn nhân lực - mấu chốt của mọi sự thành công.</p>
+            </div>
+            <div className="about-content">
+                <p>FAHASA có hơn 2.200 CB-CNV, trình độ chuyên môn giỏi, nhiệt tình, năng động, chuyên nghiệp. Lực lượng quản lý FAHASA có thâm niên công tác, giỏi nghiệp vụ nhiều kinh nghiệm, có khả năng quản lý tốt và điều hành đơn vị hoạt động hiệu quả.</p>
+            </div>
+            <div className="about-content">
+                <p>Kết hợp tuyển dụng nguồn nhân lực đầu vào có chất lượng và kế hoạch bồi dưỡng kiến thức, rèn luyện bổ sung các kỹ năng và chuẩn bị đội ngũ kế thừa theo hướng chính qui thông qua các lớp học ngắn hạn, dài hạn; các lớp bồi dưỡng CB-CNV được tổ chức trong nước cũng như ở nước ngoài đều được lãnh đạo FAHASA đặc biệt quan tâm và tạo điều kiện triển khai thực hiện. Chính vì thế, trình độ chuyên môn của đội ngũ CB-CNV ngày càng được nâng cao, đáp ứng nhu cầu ngày càng tăng của công việc cũng như sự phát triển của xã hội đang trên đường hội nhập.</p>
+            </div>
+            <div className="about-header">
+                <strong>Về hàng hóa</strong>
+            </div>
+            <div className="about-content">
+                <p>Công ty FAHASA chuyên kinh doanh: sách quốc văn, ngoại văn, văn hóa phẩm, văn phòng phẩm, dụng cụ học tập, quà lưu niệm, đồ chơi dành cho trẻ em… Một số Nhà sách trực thuộc Công ty FAHASA còn kinh doanh các mặt hàng siêu thị như: hàng tiêu dùng, hàng gia dụng, hóa  mỹ phẩm…</p>
+            </div>
+            <div className="about-content">
+                <p>Để xây dựng Thương hiệu mạnh, một trong những định hướng quan trọng hàng đầu của FAHASA là chiến lược phát triển nguồn nhân lực - mấu chốt của mọi sự thành công.</p>
+            </div>
+            <div className="about-content">
+                <p>Sách quốc văn với nhiều thể loại đa dạng như sách giáo khoa – tham khảo, giáo trình, sách học ngữ, từ điển, sách tham khảo thuộc nhiều chuyên ngành phong phú: văn học, tâm lý – giáo dục, khoa học kỹ thuật, khoa học kinh tế - xã hội, khoa học thường thức, sách phong thủy, nghệ thuật sống, danh ngôn, sách thiếu nhi, truyện tranh, truyện đọc, từ điển, công nghệ thông tin, khoa học – kỹ thuật, nấu ăn, làm đẹp...  của nhiều Nhà xuất bản, nhà cung cấp sách có uy tín như: NXB Trẻ, Giáo Dục, Kim Đồng, Văn hóa -Văn Nghệ, Tổng hợp TP.HCM, Chính Trị Quốc Gia; Công ty Đông A, Nhã Nam, Bách Việt, Alphabook, Thái Hà, Minh Lâm, Đinh Tị, Minh Long, TGM, Sáng Tạo Trí Việt, Khang Việt, Toàn Phúc…</p>
+            </div>
+            <div className="about-content">
+                <p>Sách ngoại văn bao gồm: từ điển, giáo trình, tham khảo, truyện tranh thiếu nhi , sách học ngữ, từ vựng, ngữ pháp, luyện thi TOEFL, TOEIC, IELS…được nhập từ các NXB nước ngoài như: Cambridge, Mc Graw-Hill, Pearson Education, Oxford, Macmillan, Cengage Learning…</p>
+            </div>
+            <div className="about-content">
+                <p>Văn phòng phẩm, dụng cụ học tập, đồ chơi dành cho trẻ em, hàng lưu niệm… đa dạng, phong phú, mẫu mã đẹp, chất lượng tốt, được cung ứng bởi các công ty, nhà cung cấp uy tín như: Thiên Long, XNK Bình Tây, Hạnh Thuận, Ngô Quang, Việt Văn, Trương Vui, Hương Mi, Phương Nga, Việt Tinh Anh, Chăm sóc trẻ em Việt, Mẹ và em bé…</p>
+            </div>
+            <div className="about-content">
+                <p>Cùng với việc phát hành độc quyền nhiều ấn bản các loại của các Nhà xuất bản là năng lực in ấn, sản xuất cung ứng nguồn hàng của Xí nghiệp in FAHASA, đã giúp Công ty luôn chủ động được nguồn hàng, nhất là các mặt hàng độc quyền như: lịch bloc, tập học sinh, sổ tay cao cấp, agenda, văn phòng phẩm, dụng cụ học tập…</p>
             </div>
         </div>
     )
